a8812b15492108f9e4985a8824268a5ca0701e99
guinovart1961 <42558939+guinovart1961@users.noreply.github.com>
2022-08-09 15:02:43 -0400

    Add files via upload

--- BifasicoAHMelastico_Squared_2022.m
+++ BifasicoAHMelastico_Squared_2022.m
@@ -1,7 +1,7 @@
 %Resuelve el problema elástico bifásico fibroso con simetria cuadrada
 %      
-
-clear
+clearvars;close all;
+clear all
 ang=9; % 90
 load Sk_Tk_304045506070758090_ENoP_E
 
@@ -9,10 +9,11 @@ load Sk_Tk_304045506070758090_ENoP_E
 %epoxy                          Glass
 Ea1= 3.45;                 Ea2= 73.1;                   
 Et1= 3.45;                 Et2= 73.1;                    
-Ga1= 1.2778;               Gt2= 29.9590;               
+Ga1= 1.2778;               Ga2=29.9590; 
+Gt1=1.2778;                Gt2= 29.9590;               
 Pa1= 0.35 ;                Pa2= 0.22;                                    
 Pt1= 0.35;                 Pt2= 0.22;                    
-Gt1=Et1/(2*(1+Pt1)); Ga2=Ea1/(2*(1+Pa1)); 
+                           
 
 %*************************************************************
 %INTRODUCCION DE LOS DATOS DE LA MATRIZ                      *
@@ -37,7 +38,7 @@ lam2=[0:.01:.1, .15:.05:.75, .78 .784]; %%%input('Fracción Volumétrica de la Fib
 for j=1:length(lam2)
 %**********************************************
 %RR=AHMelasticoSq(cm,cf,lam2(j));
-RR=AHMelasticoSqr_2010(cm,cf,lam2(j),Sk(:,ang),Tk(:,ang),gam1(ang),delta1(ang),theta(ang));
+RR=AHMelasticoSqr_2010(cm,cf,lam2(j),Sk(:,ang),Tk(:,ang),gam1(ang),theta(ang));
 CE(j,:)=[RR];
 CE1(j,:)=[lam2(j) RR]; % para la salida de los coeficietes
 
@@ -49,8 +50,8 @@ end
 % Para hallar las Constantes Ingenieriles Efectivas
 
 for j=1:length(lam2)
-% EE1=ingenieriles3_Hashin79(CE(j,:)); % formulas en funcion de las contantes elasticas 
-EE1=ingenieriles3(CE(j,:)); % 
+EE1=ingenieriles3_Hashin79(CE(j,:)); % formulas en funcion de las contantes elasticas 
+%EE1=ingenieriles3(CE(j,:)); % 
 EE(j,:)=EE1; %Constantes Ingenieriles Efectivas
 end
 
@@ -71,10 +72,13 @@ end
 disp('         ***********************************************')
 disp('            Coeficientes Efectivos Square Symmetry')
 disp('***********************************************************************')
-disp('    Vf        C11       C12         C13        C33       C44         C66 ')
+disp('    Vf        C11       C12         C13        C33       C44         C66    (c11-c12)/2')
 disp('***********************************************************************')
 disp(CE1)
 
+%%%% para hacer la tabla
+Taa = table(CE1);
+writetable(Taa,'Coeficientes_Efectivos.xlsx')
 
 % Salida
 disp('         ***********************************************')
@@ -84,6 +88,9 @@ disp('    Vf        LEa       Ea         UEa        LEt       Et         UEt ')
 disp('***********************************************************************')
 disp(CHE)
 
+%%%% para hacer la tabla
+Taa = table(CHE);
+writetable(Taa,'Modulos_Young.xlsx')
 
 % Salida
 disp('         ***********************************************')
@@ -93,6 +100,11 @@ disp('    Vf        LGa       Ga         UGa        LGt       Gt         UGt ')
 disp('***********************************************************************')
 disp(CHG)
 
+%%%% para hacer la tabla
+Taa = table(CHG);
+writetable(Taa,'Modulos_Shear.xlsx')
+
+
 % Salida
 disp('         ***********************************************')
 disp('            Cotas de Hashin 1979 (Modulos de Poison)')
@@ -101,44 +113,91 @@ disp('    Vf        LPa       Pa         UPa        LPt       Pt         UPt ')
 disp('***********************************************************************')
 disp(CHP)
 
- h=figure;
+%%%% para hacer la tabla
+Taa = table(CHP);
+writetable(Taa,'Modulos_Poison.xlsx')
+
+XGA=[0.48 0.52 0.546 0.56 0.6 0.656 0.672 0.62 0.65 0.66];
+
+YGA=[2.4615 3.0769 3.1538 3.3077 3.2308 3.6308 4.0461 ...
+     4.3846 4.6923 4.8461];
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+Et_IJSS=[0.437837838	2.285714286
+0.516216216	3.371428571
+0.548648649	4.685714286
+0.559459459	4.057142857
+0.567567568	4.628571429
+0.57027027	3.657142857
+0.581081081	3.257142857
+0.589189189	4.228571429
+0.6	3.714285714
+0.610810811	4.914285714
+0.637837838	4.971428571
+0.651351351	6.114285714
+0.651351351	4.228571429
+0.656756757	4.685714286
+0.662162162	5.085714286
+0.691891892	5.714285714];
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+%  h=figure;
+ figure(1)
   %*********** Comparación E_{1} 
   subplot(3,2,1)
 plot(lam2,CHE(:,4)/Im(1),'k-.',lam2,CHE(:,3)/Im(1),'r-',...
     lam2,CHE(:,2)/Im(1),'k:')
-ylabel('E_{a}/E_1(GPa)')
-% axis([0 .8  0  380])
-legend('CSH','MHA','CIH')
+ylabel('$\bar{E_{a}}/E_1$','fontsize',12,'interpreter','latex')
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
+legend('Upper bound Hashin','AHM','Lower bound Hashin','fontsize',10,'interpreter','latex')
 
  subplot(3,2,2)
+%  hold on
+plot(Et_IJSS(:,1),Et_IJSS(:,2),'ko')
+hold on
 plot(lam2,CHE(:,7)/Im(2),'k-.',lam2,CHE(:,6)/Im(2),'r-',...
     lam2,CHE(:,5)/Im(2),'k:')
-ylabel('E_{t}/E_1(GPa)')
-%axis([0 .7  0  150])
-
+hold off
+ylabel('$\bar{E_{t}}/E_1$','fontsize',12,'interpreter','latex')
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
+legend('Experiments Tsai and Hahn (1980)','fontsize',10,'interpreter','latex')
 
   %*********** Comparación G 
   subplot(3,2,3)
+ plot(XGA,YGA,'ko')
+ hold on
 plot(lam2,CHG(:,4)/Im(3),'k-.',lam2,CHG(:,3)/Im(3),'r-',...
     lam2,CHG(:,2)/Im(3),'k:')
-ylabel('G_{a}/G_1(GPa)')
-%axis([0 .7  0  150])
+hold off
+ylabel('$\bar{G_{a}}/G_1$','fontsize',12,'interpreter','latex')
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
+legend('Experiments Tsai and Hahn (1980)','fontsize',10,'interpreter','latex')
 
  subplot(3,2,4)
-plot(lam2,CHG(:,7)/Im(4),'k-.',lam2,CHG(:,6)/Im(4),'r-',lam2,CE(:,7)/Im(4),'r:',...
-    lam2,CHG(:,5)/Im(4),'k:')
-ylabel('G_{t}/G_1, m^{*}/G_1(GPa)')
-%axis([0 .7  0  150])
+plot(lam2,CE(:,7)/Im(4),'r-',lam2,CE(:,6)/Im(4),'r-.',lam2,CHG(:,7)/Im(4),'k-.',lam2,CHG(:,5)/Im(4),'k:')
+ylabel('$\bar{G_{t}}/G_1$','fontsize',12,'interpreter','latex')
+legend( '$\bar{m\acute{}}/G_1$' , '$\bar{m}/G_1$','fontsize',10,'interpreter','latex')
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
 
   %*********** Comparación \nu 
   subplot(3,2,5)
 plot(lam2,CHP(:,4)/Im(5),'k-.',lam2,CHP(:,3)/Im(5),'r-',...
     lam2,CHP(:,2)/Im(5),'k:')
-xlabel('Volumen de fibra (V_{2})') ,ylabel('\nu_{a}/\nu_{1}')
-%axis([0 .7  0  150])
+xlabel('Fiber volume fraction ($V_{2}$)','fontsize',12,'interpreter','latex')
+ylabel('$\bar{\nu_{a}}/\nu_{1}$','fontsize',12,'interpreter','latex')
+
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
 
  subplot(3,2,6)
 plot(lam2,CHP(:,7)/Im(6),'k-.',lam2,CHP(:,6)/Im(6),'r-',...
     lam2,CHP(:,5)/Im(6),'k:')
-xlabel('Volumen de fibra (V_{2})') ,ylabel('\nu_{t}/\nu_{1}')
-%axis([0 .7  0  150])
+xlabel('Fiber volume fraction ($V_{2}$)','fontsize',12,'interpreter','latex')
+ylabel('$\bar{\nu_{t}}/\nu_{1}$','fontsize',12,'interpreter','latex')
+xlim([0 .784])
+xticks([0:.2:.6,0.784])
